d9b48e5caac2c9d0d3186e812bae9829f8966f7d
Daniel Wahyu <dwahy001@sledge.cs.ucr.edu>
2018-11-26 23:26:06 -0800

    lab3 commit

--- exec.c
+++ exec.c
@@ -63,10 +63,11 @@ exec(char *path, char **argv)
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
   sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+  uint stackBase = KERNBASE - PGSIZE;
+  if((sz = allocuvm(pgdir, stackBase, stackBase + 8)) == 0)
     goto bad;
-  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
-  sp = sz;
+ // clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
+  sp = USTACKBASE;
 
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
@@ -97,6 +98,7 @@ exec(char *path, char **argv)
   oldpgdir = curproc->pgdir;
   curproc->pgdir = pgdir;
   curproc->sz = sz;
+  curproc->szStack = 1;
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
   switchuvm(curproc);
